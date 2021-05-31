cbc8cafa65d80ea8e6b3a4bcde7219373497376b
Jack Hewitt <hewittjack2011@gmail.com>
2021-05-31 10:08:35 +0100

    Formatting project and reducing the height of sidebar component

--- src/components/SignInForm.jsx
+++ src/components/SignInForm.jsx
@@ -1,83 +1,72 @@
-import React, { useState, useContext } from 'react'
-import './SignInForm.css'
-import logo from '../images/InterviewLogo.PNG'
-import { SignInContext } from '../context'
+import React, { useState, useContext } from "react";
+import "./SignInForm.css";
+import logo from "../images/InterviewLogo.PNG";
+import { SignInContext } from "../context";
 
 function SignInForm() {
-const {loggedIn, setLoggedIn} = useContext(SignInContext)
-const [passwordRecover, setPasswordRecover] = useState(false)
+  const { loggedIn, setLoggedIn } = useContext(SignInContext);
+  const [passwordRecover, setPasswordRecover] = useState(false);
 
-const handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     setLoggedIn(true);
-}
+  };
 
-const handleRecover = (e) => {
+  const handleRecover = (e) => {
     e.preventDefault();
-    console.log('recovery')
+    console.log("recovery");
+  };
+
+  return (
+    <>
+      <article className="form-container">
+        <img className="form-logo" src={logo} alt="Interview Logo" />
+        {passwordRecover ? (
+          <>
+            <form classname="form" id="recoverForm" onSubmit={handleRecover}>
+              <div className="input-container">
+                <label htmlFor="email">EMAIL ADDRESS</label>
+                <input type="email" id="email" name="email" required />
+              </div>
+              <div className="input-container">
+                <h3>
+                  Please enter the email address you would like the recovery
+                  link sent to{" "}
+                </h3>
+              </div>
+            </form>
+            <button className="btn" type="submit" form="recoverForm">
+              Recover
+            </button>
+          </>
+        ) : (
+          <>
+            <form id="loginForm" onSubmit={handleSubmit}>
+              <div className="input-container">
+                <label htmlFor="email">EMAIL ADDRESS</label>
+                <input type="email" id="email" name="email" required />
+              </div>
+              <div className="input-container">
+                <label htmlFor="password">PASSWORD</label>
+                <input type="password" id="password" name="password" required />
+              </div>
+            </form>
+            <div>
+              <button className="btn" type="submit" form="loginForm">
+                LOG IN
+              </button>
+              <button
+                className="text-btn"
+                onClick={(e) => setPasswordRecover(true)}
+              >
+                Forgot your password?
+              </button>
+            </div>
+          </>
+        )}
+      </article>
+    </>
+  );
 }
 
-
-
-    return (
-        <>
-            <article className='form-container'>
-                <img className='form-logo' src={logo} alt='Interview Logo' />
-               {passwordRecover? 
-               <>
-                    <form classname='form' id='recoverForm' onSubmit={handleRecover}>
-                    <div className='input-container'>
-                    <label htmlFor='email'>EMAIL ADDRESS</label>
-                    <input 
-                        type='email'
-                        id='email'
-                        name='email'
-                        required
-                        />
-                        </div>
-                        <div className='input-container'>
-                   <h3>Please enter the email address you would like the recovery link sent to </h3>
-                   </div>
-                       
-                </form>
-                <button className='btn' type='submit' form='recoverForm'>Recover</button>
-                   </> : 
-               <>
-                   <form id='loginForm' onSubmit={handleSubmit}>
-                       <div className='input-container'>
-                    <label htmlFor='email'>EMAIL ADDRESS</label>
-                    <input 
-                        type='email'
-                        id='email'
-                        name='email'
-                        required
-                        />
-                        </div>
-                        <div className='input-container'>
-                    <label htmlFor='password'>PASSWORD</label>    
-                    <input
-                        type='password'
-                        id='password'
-                        name='password'                        
-                        required
-                        />
-                        </div>
-                    
-                      
-                </form>
-                <div>
-                <button className='btn' type='submit' form='loginForm'>LOG IN</button>
-                <button className='text-btn' onClick={(e) => setPasswordRecover(true)}>Forgot your password?</button>  
-
-                </div>
-                   </>} 
-               
-               
-                
-              
-            </article>
-        </>
-    )
-}
-
-export default SignInForm
+export default SignInForm;
