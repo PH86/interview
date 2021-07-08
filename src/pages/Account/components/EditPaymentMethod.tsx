604b9b3f3e13e81cd9683b5b96e0a8391c78bc65
hrjuggins <hrjuggins@gmail.com>
2021-07-08 15:24:04 +0100

    Add sample features to user settings page
    More css fixes...

--- src/pages/Account/components/EditPaymentMethod.tsx
+++ src/pages/Account/components/EditPaymentMethod.tsx
@@ -4,14 +4,20 @@ export const EditPaymentMethod = () => {
     return (
         <div className="packages-page-container">
         <h1>Payment Method</h1>
-        <h2>Edit</h2>
         <form className="modal-form card-form">
-                <input className="card-number" name="card-number" type="text" maxLength={19} placeholder="Card number"/>
-                <div>
-                    <input className="expiry-date" name="expiry-date" type="text" maxLength={5} placeholder="Expiry date"/>
-                    <input className="cvv-number" name="cvv" type="text" maxLength={3} placeholder="CVV" /> 
-                </div>
-           <button type="submit" className="standard-button">Save</button>
+            <input className="card-number" name="card-number" type="text" maxLength={19} placeholder="Card number"/>
+            <div>
+                <input className="expiry-date" name="expiry-date" type="text" maxLength={5} placeholder="Expiry date"/>
+                <input className="cvv-number" name="cvv" type="text" maxLength={3} placeholder="CVV" /> 
+            </div>
+            <div>
+                <button type="submit" className="standard-button">
+                    Save
+                </button>
+                <button type="submit" className="standard-button text-button">
+                    Undo changes
+                </button>
+            </div>  
         </form>
     </div>
     )
--- src/pages/UserSettings/UserSettings.tsx
+++ src/pages/UserSettings/UserSettings.tsx
@@ -1,42 +1,88 @@
+import { useState } from "react";
 import { motion } from "framer-motion";
-import { pageTransitions } from "utils/Animations";
+import Modal from "styled-react-modal";
 
-let easing = [0.6, -0.05, 0.01, 0.99];
+import { backgroundColor, textColor } from "themes/theme";
+import { pageTransitions, modalTransitions } from "utils/Animations";
+import { MetaInfo } from "components";
+import {
+  AccountInformation,
+  SecurityInformation,
+} from "pages/UserSettings/components";
 
-const stagger = {
-    animate: {
-        transition: {
-            staggerChildren: 0.05
-        }
-    }
-};
-
-const fadeInUp = {
-    initial: {
-        y: 60,
-        opacity: 0,
-        transition: { duration: 0.6, ease: easing }
-    },
-    animate: {
-        y: 0,
-        opacity: 1,
-        transition: {
-            duration: 0.6,
-            ease: easing
-        }
-    }
-};
+const StyledModal = Modal.styled`
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: ${backgroundColor};
+    color: ${textColor};
+    border-radius: 15px;
+`;
 
 export const UserSettings: React.FC<{}> = (): React.ReactElement => {
-    return (
-        <motion.div
-            initial="initial"
-            animate="animate"
-            exit="initial"
-            variants={pageTransitions}
-            className="content-container"
-        >
-            <h1>User Settings</h1>
-        </motion.div>
-    )
-}
+  const [openModal, setOpenModal] = useState<boolean>(false);
+  const [modalComponent, setModalComponent] = useState<JSX.Element>();
+
+  const toggleModal = () => {
+    setOpenModal(!openModal);
+  };
+
+  const openModalComponent = (component: JSX.Element) => {
+    setModalComponent(component);
+    setOpenModal(true);
+  };
+  return (
+    <motion.div
+      initial="initial"
+      animate="animate"
+      exit="initial"
+      variants={pageTransitions}
+      className="content-container accounts-content-container"
+    >
+      <h1>User Settings</h1>
+      <div className="section-container">
+        <div className="detail-box sub-box">
+          <p>
+            <b>Account Information</b>
+          </p>
+          <MetaInfo label="First name" text="Test" />
+          <MetaInfo label="Last name" text="User" />
+          <MetaInfo label="Email address" text="test.user@company.com" />
+          <MetaInfo label="Phone number" text="07900000000" />
+          <MetaInfo label="Job position" text="Hiring Manager" />
+           <button
+            className="standard-button"
+            onClick={() => openModalComponent(<AccountInformation />)}
+          >
+            Edit
+          </button>
+        </div>
+        <div className="detail-box sub-box">
+          <p>
+            <b>Security Information</b>
+          </p>
+          <MetaInfo label="Password" text="**********" />
+          <button
+            className="standard-button"
+            onClick={() => openModalComponent(<SecurityInformation />)}
+          >
+            Edit
+          </button>
+        </div>
+      </div>
+      <StyledModal
+        isOpen={openModal}
+        onBackgroundClick={toggleModal}
+        onEscapeKeydown={toggleModal}
+      >
+        <motion.div variants={modalTransitions}>{modalComponent}</motion.div>
+      </StyledModal>
+    </motion.div>
+  );
+};
