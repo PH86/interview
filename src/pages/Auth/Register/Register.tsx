16530cdf5a19ddd852c5e52579f8a29fbb2cd450
ph86 <peter.hodgson2011@gmail.com>
2021-08-12 12:02:48 +0100

    INT-41 create account now calling back end, password validation added, also check if email address is already in use

--- src/pages/Auth/Register/Register.tsx
+++ src/pages/Auth/Register/Register.tsx
@@ -1,15 +1,43 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
 import { motion } from "framer-motion";
 
+import Modal from "styled-react-modal";
+
+import { backgroundColor, textColor } from "themes/theme";
 import logo from "images/interview-dark.svg";
 import { pageTransitions } from "utils/Animations";
 import searchIcon from "images/searchIcon.png";
 import { apiUrl, url } from "utils/constants";
 
+const StyledModal = Modal.styled`
+  top: '50%',
+  left: '50%',
+  right: 'auto',
+  bottom: 'auto',
+  marginRight: '-50%',
+  transform: 'translate(-50%, -50%)',
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${backgroundColor};
+  color: ${textColor};
+  border-radius: 15px;
+  max-width: 95%;
+`;
+
 export const Register: React.FC = (): React.ReactElement => {
   const [userDetails, setUserDetails] = useState({ email: "", password: "" });
   const [apiError, setApiError] = useState("");
+  const [openSuccessModal, setOpenSucessModal] = useState(false);
+  const [passwordValid, setPasswordValid] = useState(false);
+
+  const history = useHistory();
+
+  const toggleSuccessModal = () => {
+    setOpenSucessModal(!openSuccessModal);
+  };
 
   const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setUserDetails((prev) => ({
@@ -18,9 +46,25 @@ export const Register: React.FC = (): React.ReactElement => {
     }));
   };
 
+  const checkPassword = () => {
+    if (userDetails.password.length >= 8) {
+      setPasswordValid(true);
+    } else {
+      setPasswordValid(false);
+    }
+  };
+
+  useEffect(() => {
+    checkPassword();
+  }, [userDetails.password]);
+
+  // function to check regex
+  // set state when regex complies
+  // if state is true allow submit
+
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    fetch(`${process.env.REACT_APP_API_URL}${apiUrl.auth.signIn}`, {
+    fetch(`${process.env.REACT_APP_API_URL}${apiUrl.auth.createUser}`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -31,14 +75,18 @@ export const Register: React.FC = (): React.ReactElement => {
         // Handle successful api call
         if (res.ok) {
           res.json();
+          toggleSuccessModal();
           // Show error message
         } else {
-          setApiError("There was a problem with your request");
+          setApiError("Email already in use");
         }
       })
       .catch((err) => err);
-    // Remove this line once api call is set up as this is currently bypassing everything above
-    // .finally(() => signIn("test"));
+  };
+
+  const completeLogin = () => {
+    setOpenSucessModal(false);
+    history.push(url.signIn);
   };
 
   return (
@@ -54,18 +102,6 @@ export const Register: React.FC = (): React.ReactElement => {
           <img className="form-logo" src={logo} alt="Interview Logo" />
           {apiError && <p className="error-message">{apiError}</p>}
           <form id="loginForm" onSubmit={handleSubmit}>
-            <div className="input-container">
-              <label htmlFor="name">
-                Your name
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  onChange={(e) => handleOnChange(e)}
-                  required
-                />
-              </label>
-            </div>
             <div className="input-container">
               <label htmlFor="email">
                 Email address
@@ -89,10 +125,19 @@ export const Register: React.FC = (): React.ReactElement => {
                   required
                 />
               </label>
+              {!passwordValid && userDetails.password && (
+                <p>* password must be at least 8 characters long</p>
+              )}
             </div>
-            <button className="button" type="submit" form="loginForm">
-              CREATE ACCOUNT
-            </button>
+            {passwordValid ? (
+              <button className="button" type="submit" form="loginForm">
+                CREATE ACCOUNT
+              </button>
+            ) : (
+              <button className="button-grey" type="button">
+                CREATE ACCOUNT
+              </button>
+            )}
           </form>
         </article>
         <div className="form-container-footer">
@@ -112,6 +157,25 @@ export const Register: React.FC = (): React.ReactElement => {
           seamlessly uniting the advantages of our state of the art technology
         </h6>
       </div>
+      <StyledModal
+        isOpen={openSuccessModal}
+        onBackgroundClick={toggleSuccessModal}
+        onEscapeKeydown={toggleSuccessModal}
+      >
+        <article className="register-modal-container">
+          <h3 className="register-modal-title">Success!</h3>
+          <p className="register-modal-text">
+            You are now registered please proceed to log in
+          </p>
+          <button
+            onClick={() => completeLogin()}
+            className="standard-button"
+            type="button"
+          >
+            OK
+          </button>
+        </article>
+      </StyledModal>
     </motion.div>
   );
 };
